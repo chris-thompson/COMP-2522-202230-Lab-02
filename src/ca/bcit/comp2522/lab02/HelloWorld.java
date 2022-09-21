17cf3e3138222c9f8e05ec9bca6fa41b866c159e
Christopher Thompson <chris_thompson@bcit.ca>
2022-09-20 22:10:05 -0700

    Hello Lab 02!

---
+++ src/ca/bcit/comp2522/lab02/HelloWorld.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2522.lab02;
+
+/**
+ * A silly class you can delete.
+ *
+ * @author Chris
+ * @version 2022
+ */
+public final class HelloWorld {
+
+    private HelloWorld() { }
+
+    /**
+     * Drives the program.
+     * @param args unused
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello world");
+    }
+}
